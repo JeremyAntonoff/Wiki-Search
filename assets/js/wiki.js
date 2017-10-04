b2cd7dbadec79201d2510dd6e920fbd6f500e61d
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-10-04 15:32:42 -0400

    initial commit

---
+++ assets/js/wiki.js
@@ -0,0 +1,52 @@
+var res = []
+var input = $("input[type=text]")
+var searchItem;
+$(document).ready(function() {
+	inputSearch();
+});
+
+function searchwiki() {
+	$.ajax({
+		url: ("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" + searchItem + "&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&callback=?"),
+		dataType: "json",
+		headers: { 'Api-User-Agent': 'Example/1.0' },
+		success: function(results) {
+			res = []
+			var array = Object.keys(results.query.pages).map(function(key) {
+				return key
+			});
+			for (var i = 0; i < array.length;i++) {
+				res.push({
+					title: results.query.pages[array[i]].title,
+					pageid: results.query.pages[array[i]].pageid,
+					content: results.query.pages[array[i]].extract
+				});
+			}
+			listItems(res);
+		}
+	})
+}
+
+function inputSearch() {
+	input.on("keypress", function(e){
+		searchItem = input.val();
+		if (e.which === 13) {
+			removeList();
+			return searchwiki();
+		}
+	})
+}
+
+function listItems() {
+	console.log(res)
+
+	for (var i =0; i < res.length; i++) {
+		$("ul").append("<li>" + "<span id = 'title'>" + "<a href = https://en.wikipedia.org/?curid=" + res[i].pageid + ">"
+		+ res[i].title + "</a>" + "</span>" + "<br><br>" +  res[i].content + "</li>")
+	}
+}
+
+function removeList() {
+	$("ul").html(" ");
+}
+
