53485b66645c3e3f31cb04e0b1d3f5d4adc388b4
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-04 02:45:45 -0400

    add es6 arrow functions and promises

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -10,25 +10,28 @@ function searchwiki() {
 		url: ("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" + searchItem + "&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&callback=?"),
 		dataType: "json",
 		headers: { 'Api-User-Agent': 'Example/1.0' },
-		success: function(results) {
-			res = []
-			var array = Object.keys(results.query.pages).map(function(key) {
-				return key
+	}).then((results) => {
+		res = []
+		var array = Object.keys(results.query.pages).map(function(key) {
+			return key
+		});
+		for (var i = 0; i < array.length; i++) {
+			res.push({
+				title: results.query.pages[array[i]].title,
+				pageid: results.query.pages[array[i]].pageid,
+				content: results.query.pages[array[i]].extract
 			});
-			for (var i = 0; i < array.length;i++) {
-				res.push({
-					title: results.query.pages[array[i]].title,
-					pageid: results.query.pages[array[i]].pageid,
-					content: results.query.pages[array[i]].extract
-				});
-			}
-			listItems(res);
 		}
+		listItems(res);
+	})
+	.catch((err) => {
+		$("ul").append("<li><p>Something went wrong. Please try again later.</li>")
+		$('li').css({'color': 'red', 'text-align': 'center'})
 	})
 }
 
 function inputSearch() {
-	input.on("keypress", function(e){
+	input.on("keypress", (e) => {
 		searchItem = input.val();
 		if (e.which === 13) {
 			removeList();
@@ -38,8 +41,6 @@ function inputSearch() {
 }
 
 function listItems() {
-	console.log(res)
-
 	for (var i =0; i < res.length; i++) {
 		$("ul").append("<li>" + "<span id = 'title'>" + "<a href = https://en.wikipedia.org/?curid=" + res[i].pageid + ">"
 		+ res[i].title + "</a>" + "</span>" + "<br><br>" +  res[i].content + "</li>")
