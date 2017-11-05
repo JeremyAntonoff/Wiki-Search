605159f35c68b5edaff63f08b359dda748dcd2fb
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-11-05 13:03:23 -0500

    change to es6 template literal

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,53 +1,60 @@
-var res = []
-var input = $("input[type=text]")
+var res = [];
+var input = $('input[type=text]');
 var searchItem;
 $(document).ready(function() {
-	inputSearch();
+  inputSearch();
 });
 
 function searchwiki() {
-	$.ajax({
-		url: ("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=" + searchItem + "&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&callback=?"),
-		dataType: "json",
-		headers: { 'Api-User-Agent': 'Example/1.0' },
-	}).then((results) => {
-		res = []
-		var array = Object.keys(results.query.pages).map(function(key) {
-			return key
-		});
-		for (var i = 0; i < array.length; i++) {
-			res.push({
-				title: results.query.pages[array[i]].title,
-				pageid: results.query.pages[array[i]].pageid,
-				content: results.query.pages[array[i]].extract
-			});
-		}
-		listItems(res);
-	})
-	.catch((err) => {
-		$("ul").append("<li><p>Something went wrong. Please try again later.</li>")
-		$('li').css({'color': 'red', 'text-align': 'center'})
-	})
+  $.ajax({
+    url:
+      'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=' +
+      searchItem +
+      '&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max&callback=?',
+    dataType: 'json',
+    headers: { 'Api-User-Agent': 'Example/1.0' }
+  })
+    .then(results => {
+      res = [];
+      var array = Object.keys(results.query.pages).map(function(key) {
+        return key;
+      });
+      for (var i = 0; i < array.length; i++) {
+        res.push({
+          title: results.query.pages[array[i]].title,
+          pageid: results.query.pages[array[i]].pageid,
+          content: results.query.pages[array[i]].extract
+        });
+      }
+      listItems(res);
+    })
+    .catch(err => {
+      $('ul').append(
+        '<li><p>Something went wrong. Please try again later.</li>'
+      );
+      $('li').css({ color: 'red', 'text-align': 'center' });
+    });
 }
 
 function inputSearch() {
-	input.on("keypress", (e) => {
-		searchItem = input.val();
-		if (e.which === 13) {
-			removeList();
-			return searchwiki();
-		}
-	})
+  input.on('keypress', e => {
+    searchItem = input.val();
+    if (e.which === 13) {
+      removeList();
+      return searchwiki();
+    }
+  });
 }
 
 function listItems() {
-	for (var i =0; i < res.length; i++) {
-		$("ul").append("<li>" + "<span id = 'title'>" + "<a href = https://en.wikipedia.org/?curid=" + res[i].pageid + ">"
-		+ res[i].title + "</a>" + "</span>" + "<br><br>" +  res[i].content + "</li>")
-	}
+  for (var i = 0; i < res.length; i++) {
+    $('ul').append(
+      `<li><span id = 'title'><a href = https://en.wikipedia.org/?curid=res${[i]
+        .pageid} >${res[i].title}</a></span><br><br>${res[i].content}</li>`
+    );
+  }
 }
 
 function removeList() {
-	$("ul").html(" ");
+  $('ul').html(' ');
 }
-
